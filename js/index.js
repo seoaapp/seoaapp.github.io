03e8a8f5b6b19166c7cff2416296e2c843930d3b
ttakkku <ttakkku@outlook.com>
2019-05-06 15:47:08 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-
+/**
+* f12 , ctrl +shift +i 막는 스크립트
+*/
+function KeyEventHandle()
+    {
+        if(
+            ( event.ctrlKey == true && ( event.keyCode == 16 || event.keyCode == 73 ) ) ||
+            ( event.keyCode >= 112 && event.keyCode <= 123 ))
+        {
+            event.keyCode                = 0;
+            event.cancelBubble        = true;
+            event.returnValue        = false;
+        }
+    }
+    document.onkeydown=KeyEventHandle;
+    document.onkeyup=KeyEventHandle;
